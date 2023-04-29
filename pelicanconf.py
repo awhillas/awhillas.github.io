6bfe2954874efac48f9958238d1a280fe0b6672e
Alexander Whillas <alex@yabble.com>
2023-04-29 19:45:09 +1200

    Fix outputting to docs/

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,6 +11,9 @@ TIMEZONE = 'Pacific/Auckland'
 
 DEFAULT_LANG = 'en'
 
+OUTPUT_PATH = 'docs/'
+OUTPUT_RETENTION = ["CNAME"]
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
